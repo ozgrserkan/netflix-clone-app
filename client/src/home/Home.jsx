bd8dd5449327387556848a2165e667448c5ae93c
serkan <ozgr.serkan09@gmail.com>
2021-12-27 12:38:48 +0300

    position fixed navbar added

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -6,6 +6,60 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+      reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit
+      amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+      reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit
+      amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+      reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit
+      amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+      reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit
+      amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+      reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor, sit
+      amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Eveniet, reiciendis! Lorem ipsum
+      dolor, sit amet consectetur adipisicing elit. Eveniet, reiciendis!
     </div>
   );
 };
