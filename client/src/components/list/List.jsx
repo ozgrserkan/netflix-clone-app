24f52a817c48b0ae2296d9c11508ac1555111619
serkan <ozgr.serkan09@gmail.com>
2021-12-28 14:59:17 +0300

    homepage hover effect completed

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -16,7 +16,7 @@ const List = () => {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-    if (direction === "right" && slideNumber < 5) {
+    if (direction === "right" && slideNumber < 2) {
       setSlideNumber(slideNumber + 1);
 
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
@@ -33,16 +33,16 @@ const List = () => {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <ArrowForwardIosOutlinedIcon
           className="sliderArrow right"
