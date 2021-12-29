68a7447015d858df8857000306800947fa9c8e5e
serkan <ozgr.serkan09@gmail.com>
2021-12-29 16:07:27 +0300

    register page completed

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./register.scss";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [password, setPassword] = useState("");
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
   return (
     <div className="register">
       <div className="top">
-        <img
-          className="logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-          alt=""
-        />
-        <button className="loginButton">Sign In</button>
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
       </div>
       <div className="container">
         <h1>Unlimited movies, TV shows, and more.</h1>
@@ -18,6 +30,21 @@ const Register = () => {
         <p>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="passwrod" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
