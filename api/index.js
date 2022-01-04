66eaea36b690b7d5707d0469f5a80c4b9918dfcb
serkan <ozgr.serkan09@gmail.com>
2022-01-04 14:11:12 +0300

    node js register and login with jwt added

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.listen(5000, () => {
+  console.log("Backend server is running!");
+});
---
+++ api/models/List.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: true, unique: true },
+    type: { type: String },
+    genre: { type: String },
+    isSeries: { type: Boolean, default: false },
+    content: { type: Array },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("List", ListSchema);
