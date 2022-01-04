bb1b3219a5cecff519307a18175b9c2b9fdf4eeb
serkan <ozgr.serkan09@gmail.com>
2022-01-04 17:23:26 +0300

    user crud operations added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 app.listen(5000, () => {
   console.log("Backend server is running!");
 });
