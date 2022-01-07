a49c5539a0941dd2a9d6f186e0a79b89b21f58b6
serkan <ozgr.serkan09@gmail.com>
2022-01-07 17:24:05 +0300

    rest api movie list

--- api/models/List.js
+++ api/models/List.js
@@ -5,7 +5,6 @@ const ListSchema = new mongoose.Schema(
     title: { type: String, require: true, unique: true },
     type: { type: String },
     genre: { type: String },
-    isSeries: { type: Boolean, default: false },
     content: { type: Array },
   },
   {
