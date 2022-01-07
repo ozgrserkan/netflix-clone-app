c80a5d68f7f0e57a136ed20a2d446ab4aa294e19
serkan <ozgr.serkan09@gmail.com>
2022-01-07 17:32:13 +0300

    react router dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,21 @@ import Navbar from "./components/navbar/Navbar";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/movies">
+        <Home />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 };
 
