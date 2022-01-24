f78190ef2b0629e0779253ab9cd8a8b50eb5b854
serkan <ozgr.serkan09@gmail.com>
2022-01-24 17:56:39 +0300

    fetch api

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -4,7 +4,7 @@ import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutl
 import ListItem from "../listItem.jsx/ListItem";
 import React, { useRef, useState } from "react";
 
-const List = () => {
+const List = ({ list }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [isMoved, setIsMoved] = useState(false);
   const listRef = useRef();
@@ -25,7 +25,7 @@ const List = () => {
 
   return (
     <div className="list">
-      <span className="listTitle">Continue to Watch</span>
+      <span className="listTitle">{list.title}</span>
       <div className="wrapper">
         <ArrowBackIosOutlinedIcon
           className="sliderArrow left"
@@ -33,16 +33,9 @@ const List = () => {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem index={0} />
-          <ListItem index={1} />
-          <ListItem index={2} />
-          <ListItem index={3} />
-          <ListItem index={4} />
-          <ListItem index={5} />
-          <ListItem index={6} />
-          <ListItem index={7} />
-          <ListItem index={8} />
-          <ListItem index={9} />
+          {list.content.map((item, i) => (
+            <ListItem index={i} item={item} />
+          ))}
         </div>
         <ArrowForwardIosOutlinedIcon
           className="sliderArrow right"
