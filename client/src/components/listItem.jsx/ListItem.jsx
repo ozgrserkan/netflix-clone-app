88d7d98d2c0c54d26f7fe6896abeda72665a4d8e
serkan <ozgr.serkan09@gmail.com>
2022-01-24 18:27:06 +0300

    displaying movies from api

--- client/src/components/listItem.jsx/ListItem.jsx
+++ client/src/components/listItem.jsx/ListItem.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./listItem.scss";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import AddIcon from "@mui/icons-material/Add";
 import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined";
 import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import axios from "axios";
 
-const ListItem = ({ index }) => {
+const ListItem = ({ index, item }) => {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
-
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get("/movies/find/" + item, {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDQxYjQwNjM1MTE1ZTAyZGFiOGFjYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzAzMjM1MSwiZXhwIjo2NDQyOTg0MzUxfQ.o9p6qYdzVbUrGh7xXEXmZqExBUwOJND_kVo70nr1Ckg",
+          },
+        });
+        setMovie(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMovie();
+  }, [item]);
   return (
     <div
       className="listItem"
@@ -17,13 +32,10 @@ const ListItem = ({ index }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img
-        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
-        alt=""
-      />
+      <img src={movie.img} alt="" />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={movie.trailer} autoPlay={true} loop />
           <div className="itemInfo">
             <div className="icons">
               <PlayArrowIcon className="icon" />
@@ -32,15 +44,12 @@ const ListItem = ({ index }) => {
               <ThumbUpAltOutlinedIcon className="icon" />
             </div>
             <div className="itemInfoTop">
-              <span>1 hour 14 mins</span>
-              <span className="limit">+16</span>
-              <span>1999</span>
+              <span>{movie.duration}</span>
+              <span className="limit">+{movie.limit}</span>
+              <span>{movie.year}</span>
             </div>
-            <div className="desc">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Deleniti, fugiat!
-            </div>
-            <div className="genre">Action</div>
+            <div className="desc">{movie.desc}</div>
+            <div className="genre">{movie.desc}</div>
           </div>
         </>
       )}
