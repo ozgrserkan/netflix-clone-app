9038b82e198c229af299e91cd61b0b5a228bf9d3
serkan <ozgr.serkan09@gmail.com>
2022-01-27 18:22:16 +0300

    admin dashboard edit and delete video added

---
+++ adminpanel/src/context/movieContext/apiCalls.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {
+  deleteMoviesFailure,
+  deleteMoviesStart,
+  deleteMoviesSuccess,
+  getMoviesFailure,
+  getMoviesStart,
+  getMoviesSuccess,
+} from "./MovieActions";
+
+export const getMovies = async (dispatch) => {
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("/movies", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(getMoviesSuccess(res.data));
+  } catch (err) {
+    dispatch(getMoviesFailure());
+  }
+};
+
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMoviesStart());
+  try {
+    await axios.delete("/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMoviesSuccess(id));
+  } catch (err) {
+    dispatch(deleteMoviesFailure());
+  }
+};
